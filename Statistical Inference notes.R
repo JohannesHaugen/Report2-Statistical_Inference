be0f33d138ae016b63b365c144a50f8e671727ed
Johannes Haugen <JohannesHaugen@Johanness-MBP-2.lan>
2020-10-20 10:45:01 +0200

    Oppsummeringstabell ferdig.

--- Statistical Inference notes.R
+++ Statistical Inference notes.R
@@ -1,6 +1,7 @@
 library(readr)  #Laster inn pakkene med de ulike funksjonene
 library(tidyverse)
 library(flextable)
+library(rstatix)
 
 download.file(url = "https://ndownloader.figstatic.com/files/14702420", 
               destfile = "./data/hypertrophy.csv")  #Laster ned datafilen
@@ -11,9 +12,9 @@ hypertrophy <- read_csv("./data/hypertrophy.csv")  #Laster inn datafilen og kobl
 view(hypertrophy) #Viser datafilene
 
 var_interest <- c("SUB_ID", "CLUSTER", "AGE", "T1_BODY_MASS", 
-                  "Squat_3RM_kg") # Plukker ut hvilke variabler vi er interesserte i å ha med
+                  "PERCENT_TYPE_II_T1") # Plukker ut hvilke variabler vi er interesserte i å ha med
 
-hyp <- hypertrophy %>%
+hyptable <- hypertrophy %>%
   
   select(all_of(var_interest)) %>%
   
@@ -22,7 +23,7 @@ hyp <- hypertrophy %>%
   # Navnene kommer inn i "variable" og verdier inn i "value".
   pivot_longer(names_to = "variable",
                values_to = "value",
-               cols = AGE:Squat_3RM_kg) %>%
+               cols = AGE:PERCENT_TYPE_II_T1) %>%
   
  group_by(CLUSTER, variable) %>%
   filter(!is.na(CLUSTER)) %>%  #Filtrerer vekk forsøkspersoner som ikke ble regnet som 
@@ -44,11 +45,11 @@ hyp <- hypertrophy %>%
          variable = factor(variable,
                            levels = c("AGE", # Denne delen bestemmer rekkefølgen i tabellen
                                       "T1_BODY_MASS", 
-                                      "Squat_3RM_kg"),
+                                      "PERCENT_TYPE_II_T1"),
                            
                            labels = c("Age (years)",  # Bestemmer navnene på variablene
                                       "Body mass (kg)",
-                                      "Squat 3RM (kg)"))) %>%
+                                      "Type II fiber (%)"))) %>%
   select(-m, -s) %>%   # Selekterer vekk gjennomsnittet og standardavviket
   
   pivot_wider(names_from = CLUSTER,
@@ -60,7 +61,9 @@ hyp <- hypertrophy %>%
   print() #Printer resultatet av koden over
 
 
-hyp %>%
+# Lag tabellen
+
+hyptable %>%
   flextable() %>% #Lag tabell med Flextable
   
   add_header_row(values = "Table 1", colwidths = 3) %>% #Angir tittel på tabellen
@@ -70,3 +73,47 @@ hyp %>%
   
   autofit() #Gjør tabellen penere
   
+
+########################################
+# Prepare the data for testing
+bxp <- ggboxplot(hypertrophy, x = "CLUSTER", y = "T3T1_PERCENT_CHANGE_FAST_CSA", 
+                 ylab = "Fiber type II change", xlab = "CLUSTER", add = "jitter")
+
+bxp
+
+
+df <- read_csv("./data/hypertrophy.csv") %>%
+  filter(!is.na(CLUSTER)) %>%
+  select(CLUSTER, SUB_ID, T3T1_PERCENT_CHANGE_FAST_CSA) %>%
+  group_by(CLUSTER) %>%
+  get_summary_stats(T3T1_PERCENT_CHANGE_FAST_CSA, type = "mean_sd") %>%
+  print()
+  
+df %>%
+  flextable() %>% #Lag tabell med Flextable
+  
+  set_header_labels(CLUSTER = "Cluster",
+                    variable = "Variabel", 
+                    n = "n",
+                    mean = "Gjennomsnitt",
+                    sd = "SD") %>%
+  add_header_row(values = "Summary statistics", colwidths = 5) %>% #Angir tittel på tabellen
+  
+  
+  autofit()
+
+
+bxp <- ggboxplot(df, x = "CLUSTER", y = "T3T1_PERCENT_CHANGE_FAST_CSA", 
+  ylab = "Fiber type II change", xlab = "CLUSTER", add = "jitter")
+
+bxp
+
+df %>%
+  identify_outliers(variable) %>%
+  print()
+
+
+
+
+
+
